d1cfe0f753d6b16230dbf908d611e64cbe033ad2
shivalasvegas <shiva.king@academytrainee.com>
2020-07-13 21:04:30 +0100

    ProductServices: created and modified to mirror CustomerServices

---
+++ src/main/java/com/qa/ims/services/ProductServices.java
@@ -0,0 +1,33 @@
+package com.qa.ims.services;
+
+import java.util.List;
+
+import com.qa.ims.persistence.dao.Dao;
+import com.qa.ims.persistence.domain.Product;
+
+
+public class ProductServices implements CrudServices<Product> {
+	
+private Dao<Product> productDao;
+	
+	public ProductServices(Dao<Product> productDao) {
+		this.productDao = productDao;
+	}
+	
+	public List<Product> readAll() {
+		return productDao.readAll();
+	}
+
+	public Product create(Product product) {
+		return productDao.create(product);
+	}
+
+	public Product update(Product product) {
+		return productDao.update(product);
+	}
+
+	public void delete(Long product_id) {
+		productDao.delete(product_id);
+	}
+	
+}

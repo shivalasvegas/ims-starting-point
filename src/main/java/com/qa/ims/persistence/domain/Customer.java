79ffdd55a766b0ffc7775852e90ed36a1fa15d1b
shivalasvegas <shiva.king@academytrainee.com>
2020-07-13 16:31:04 +0100

    Customer class: Added in field names- address, email and password

--- src/main/java/com/qa/ims/persistence/domain/Customer.java
+++ src/main/java/com/qa/ims/persistence/domain/Customer.java
@@ -1,11 +1,14 @@
 package com.qa.ims.persistence.domain;
 
 public class Customer {
-
+	//variables
 	private Long id;
 	private String firstName;
 	private String surname;
-
+	private String address;
+	private String email;
+	private String password;
+	//constructors
 	public Customer(String firstName, String surname) {
 		this.firstName = firstName;
 		this.surname = surname;
@@ -17,6 +20,15 @@ public class Customer {
 		this.surname = surname;
 	}
 
+	public Customer(Long id, String firstName, String surname, String address, String email, String password) {
+		this.id = id;
+		this.firstName = firstName;
+		this.surname = surname;
+		this.address = address;
+		this.email = email;
+		this.password = password;
+	}
+	// getters and setters
 	public Long getId() {
 		return id;
 	}
@@ -41,10 +53,34 @@ public class Customer {
 		this.surname = surname;
 	}
 
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	public String toString() {
 		return "id:" + id + " first name:" + firstName + " surname:" + surname;
 	}
-
+	// override methods
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -52,6 +88,9 @@ public class Customer {
 		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((surname == null) ? 0 : surname.hashCode());
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
 		return result;
 	}
 
@@ -79,6 +118,24 @@ public class Customer {
 				return false;
 		} else if (!surname.equals(other.surname))
 			return false;
+	
+		if (address == null) {
+		if (other.address != null)
+			return false;
+	} else if (!address.equals(other.address))
+	return false;
+		
+	if (email == null) {
+	if (other.email != null)
+		return false;
+	} else if (!email.equals(other.email))
+	return false;
+		
+	if (password == null) {
+	if (other.password != null)
+		return false;
+	} else if (!password.equals(other.password))
+		return false;
 		return true;
 	}
 

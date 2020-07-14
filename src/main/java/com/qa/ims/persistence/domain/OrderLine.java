33f55e2a0aa2530cb67a8aace81a0b07dcedeaed
shivalasvegas <shiva.king@academytrainee.com>
2020-07-14 14:38:43 +0100

    OrderLine: created

---
+++ src/main/java/com/qa/ims/persistence/domain/OrderLine.java
@@ -0,0 +1,19 @@
+package com.qa.ims.persistence.domain;
+
+public class OrderLine {
+	// variables
+			private Long orderLine_id;
+			private Long fk_customer_id;
+			private Long fk_order_id;
+			private Long fk_product_id;			
+			private Long product_qty;
+			private double product_total;
+
+			// constructors
+			public OrderLine() {	
+			}
+			
+			
+			
+
+}

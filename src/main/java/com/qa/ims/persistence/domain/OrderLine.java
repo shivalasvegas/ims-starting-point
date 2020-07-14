804edc5c687b63f58a8f40e98c8a2d69d519030f
shivalasvegas <shiva.king@academytrainee.com>
2020-07-14 14:50:10 +0100

    OrderLine: Added variables and constructors

--- src/main/java/com/qa/ims/persistence/domain/OrderLine.java
+++ src/main/java/com/qa/ims/persistence/domain/OrderLine.java
@@ -13,7 +13,38 @@ public class OrderLine {
 			public OrderLine() {	
 			}
 			
+			public OrderLine(Long fk_customer_id, Long fk_order_id, Long fk_product_id, Long product_qty) {
+				this.fk_customer_id = fk_customer_id;
+				this.fk_order_id = fk_order_id;
+				this.fk_product_id = fk_product_id;
+				this.product_qty = product_qty;
+	
+			}
 			
+			public OrderLine(Long orderLine_id, Long fk_customer_id, Long fk_order_id, Long fk_product_id, Long product_qty) {
+				this.orderLine_id = orderLine_id;
+				this.fk_customer_id = fk_customer_id;
+				this.fk_order_id = fk_order_id;
+				this.fk_product_id = fk_product_id;
+				this.product_qty = product_qty
+			}
+			
+			public OrderLine(Long fk_customer_id, Long fk_order_id, Long fk_product_id, Long product_qty, double product_total) {
+				this.fk_customer_id = fk_customer_id;
+				this.fk_order_id = fk_order_id;
+				this.fk_product_id = fk_product_id;
+				this.product_qty = product_qty;
+				this.product_total = product_total;
+			}
+			
+			public OrderLine(Long orderLine_id, Long fk_customer_id, Long fk_order_id, Long fk_product_id, Long product_qty, double product_total) {
+				this.orderLine_id = orderLine_id;
+				this.fk_customer_id = fk_customer_id;
+				this.fk_order_id = fk_order_id;
+				this.fk_product_id = fk_product_id;
+				this.product_qty = product_qty;
+				this.product_total = product_total;
+			}
 			
 
 }

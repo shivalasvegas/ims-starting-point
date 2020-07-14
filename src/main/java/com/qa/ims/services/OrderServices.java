919a7d964acb17a514cccb55219fe4c4c8326bec
shivalasvegas <shiva.king@academytrainee.com>
2020-07-14 13:17:55 +0100

    Added OrderServices; modified to mirror CustomerServices

---
+++ src/main/java/com/qa/ims/services/OrderServices.java
@@ -0,0 +1,31 @@
+package com.qa.ims.services;
+
+import java.util.List;
+
+import com.qa.ims.persistence.dao.Dao;
+import com.qa.ims.persistence.domain.Order;
+
+public class OrderServices implements CrudServices<Order>{
+private Dao<Order> orderDao;
+	
+	public OrderServices(Dao<Order> orderDao) {
+		this.orderDao = orderDao;
+	}
+	
+	public List<Order> readAll() {
+		return orderDao.readAll();
+	}
+
+	public Order create(Order order) {
+		return orderDao.create(order);
+	}
+
+	public Order update(Order order) {
+		return orderDao.update(order);
+	}
+
+	public void delete(Long order_id) {
+		orderDao.delete(order_id);
+	}
+
+}
